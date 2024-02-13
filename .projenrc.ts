5ade29b2befee808ed16e56e166d6dfb7d9c4a29
Kazuho CryerShinozuka <malaysia.cryer@gmail.com>
2024-02-13 23:33:44 +0900

    setup projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,16 +2,34 @@ import { awscdk } from 'projen';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Kazuho CryerShinozuka',
   authorAddress: 'malaysia.cryer@gmail.com',
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.127.0',
+  description: 'Automatic Start and Stop Scheduler for AWS RDS',
   defaultReleaseBranch: 'main',
-  jsiiVersion: '~5.0.0',
-  name: 'rds-scheduler',
+  keywords: ['aws', 'cdk', 'RDS', 'aws-cdk', 'EventBridge'],
+  jsiiVersion: '~5.3.0',
+  name: 'cdk-rds-scheduler',
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/malaysia.cryer/rds-scheduler.git',
+  repositoryUrl: 'https://github.com/badmintoncryer/cdk-rds-scheduler.git',
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  eslintOptions: {
+    dirs: [],
+    ignorePatterns: ['example/**/*', 'test/assets/**/*', 'test/*.snapshot/**/*', '*.d.ts'],
+  },
+  gitignore: ['*.js', '*.d.ts', '!test/.*.snapshot/**/*', '.tmp'],
+  tsconfigDev: {
+    compilerOptions: {},
+    exclude: ['example', 'test/.*.snapshot'],
+  },
+  devDeps: ['@aws-cdk/integ-runner@2.127.0', '@aws-cdk/integ-tests-alpha@2.127.0-alpha.0'],
+
+  packageName: 'rds-scheduler', /* The "name" in package.json. */
+
+  publishToPypi: {
+    distName: 'rds-scheduler',
+    module: 'rds_scheduler',
+  },
 });
+
+project.projectBuild.testTask.exec('yarn tsc -p tsconfig.dev.json && yarn integ-runner');
+
 project.synth();
